b59f7c534c85c3f1dd3d67cd7f00d48fa7915fee
陈寒彤 <cxwdyx620@gmail.com>
2024-02-09 19:48:33 +0800

    init(lib_rpc): adjust file struc

--- crates/libs/lib-rpc/src/lib.rs
+++ crates/libs/lib-rpc/src/lib.rs
@@ -1,1 +1,22 @@
 pub mod request;
+pub mod model;
+
+#[macro_export]
+macro_rules! parse_field {
+    ($map:expr, $field:literal) => {
+        $map.get($field)
+            .ok_or_else(|| anyhow!("{} not found", $field))?
+            .parse()
+            .map_err(|_| anyhow!("{} parse error", $field))?
+    };
+    ($map:expr, $field:literal, $type:ty) => {
+        $map.get($field)
+            .ok_or_else(|| anyhow!("{} not found", $field))?
+            .parse::<$type>()
+            .map_err(|_| anyhow!("{} parse error", $field))?
+    };
+    ($map:expr, $field:literal, $type:ty, $default:expr) => {
+        $map.get($field)
+            .map_or_else(|| $default, |v| v.parse::<$type>().unwrap_or($default))
+    };
+}
