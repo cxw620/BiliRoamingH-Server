bfb81d353e1cc861e5fb892ea79b225c21d62337
陈寒彤 <cxwdyx620@gmail.com>
2024-02-19 17:48:01 +0800

    feat(lib_rpc): add RpcBuilderT && format codes

--- crates/libs/lib-rpc/src/request/interface.rs
+++ crates/libs/lib-rpc/src/request/interface.rs
@@ -2,15 +2,15 @@ use anyhow::Result;
 use http_02::{HeaderMap as HttpHeaderMap, Method as HttpMethod};
 use url::Url;
 
-use std::{borrow::Cow, future::Future, fmt::Debug as StdDebug};
+use std::{borrow::Cow, fmt::Debug as FmtDebug, future::Future};
 
 use super::client::{
     rest::{ReqBody, RestRequest, RestRequestBuilder},
     utils::RawResponseExt,
 };
-use crate::utils::Upstream;
+use crate::utils::{ManagedHeaderMap, Upstream};
 
-/// The trait definition for a RPC request.
+/// The trait definition for a RESTful RPC request.
 pub trait RpcT<'r> {
     /// Pre-defined request method. Default to `GET`.
     const METHOD: HttpMethod = HttpMethod::GET;
@@ -23,11 +23,12 @@ pub trait RpcT<'r> {
     fn execute_rpc(
         proxy: Option<&'r str>,
         query: Option<Cow<'r, str>>,
-        headers: Option<impl Into<HttpHeaderMap> + StdDebug>,
-        body: Option<impl Into<ReqBody> + StdDebug>,
+        headers: Option<impl Into<HttpHeaderMap> + FmtDebug>,
+        body: Option<impl Into<ReqBody> + FmtDebug>,
     ) -> impl Future<Output = Result<RawResponseExt>> + Send {
-        GeneralRequest::new(Self::METHOD, Self::UPSTREAM, Self::PATH)
+        GeneralRpc::new((), Self::UPSTREAM)
             .with_proxy(proxy)
+            .with_path(Self::PATH)
             .with_query(query)
             .with_headers(headers)
             .with_body(body)
@@ -35,7 +36,62 @@ pub trait RpcT<'r> {
     }
 }
 
-pub struct GeneralRequest<'r> {
+/// The trait definition for a RPC Request builder
+pub trait RpcBuilderT<'r>: Sized {
+    /// Pre-defined request method. Default to `GET`.
+    const DEFAULT_HTTP_METHORD: HttpMethod = HttpMethod::GET;
+    /// Pre-defined request authority.
+    const DEFAULT_UPSTREAM: Upstream<'r>;
+
+    type Request;
+    type Response;
+
+    /// Create a new instance of the rpc builder
+    fn new(request: Self::Request, upstream: impl Into<Upstream<'r>>) -> Self;
+
+    /// Create a new instance of the rpc builder with default upstream
+    #[inline]
+    fn new_default_upstream(request: Self::Request) -> Self {
+        Self::new(request, Self::DEFAULT_UPSTREAM)
+    }
+
+    /// Set the method for the RPC request
+    fn with_method(self, _method: HttpMethod) -> Self {
+        self
+    }
+
+    /// Set the upstream for the RPC request
+    fn with_upstream(self, upstream: impl Into<Upstream<'r>>) -> Self;
+
+    /// Set the proxy for the RPC request
+    fn with_proxy(self, proxy: Option<&'r str>) -> Self;
+
+    /// Set the path for the RPC request
+    fn with_path(self, _path: &'r str) -> Self {
+        self
+    }
+
+    /// Set the query for the RPC request
+    fn with_query(self, _query: Option<Cow<'r, str>>) -> Self {
+        self
+    }
+
+    /// Set the headers for the RPC request
+    fn with_headers(self, headers: Option<impl Into<HttpHeaderMap>>) -> Self;
+
+    /// Set the headers(managed) for the RPC request
+    fn with_headers_managed(self, headers: Option<impl Into<ManagedHeaderMap>>) -> Self;
+
+    /// Set the body for the RPC request
+    fn with_body(self, _body: Option<impl Into<ReqBody>>) -> Self {
+        self
+    }
+
+    /// Execute the RPC request
+    fn execute(self) -> impl Future<Output = Result<Self::Response>> + Send;
+}
+
+pub struct GeneralRpc<'r> {
     /// The method of the request. Default to `GET`.
     method: HttpMethod,
     /// The authority of the request.
@@ -48,49 +104,74 @@ pub struct GeneralRequest<'r> {
     inner: RestRequestBuilder<'r>,
 }
 
-impl<'r> GeneralRequest<'r> {
+impl<'r> RpcBuilderT<'r> for GeneralRpc<'r> {
+    const DEFAULT_UPSTREAM: Upstream<'r> = Upstream::new_custom("http://no.default.upstream.com");
+
+    type Request = ();
+    type Response = RawResponseExt;
+
     #[inline]
-    pub fn new(method: HttpMethod, upstream: impl Into<Upstream<'r>>, path: &'r str) -> Self {
+    fn new(_request: Self::Request, upstream: impl Into<Upstream<'r>>) -> Self {
         Self {
-            method,
+            method: HttpMethod::GET,
             upstream: upstream.into(),
-            path,
+            path: "",
             query: None,
             inner: RestRequest::builder(),
         }
     }
 
-    /// Configure proxy for the request
     #[inline]
-    pub fn with_proxy(mut self, proxy: Option<&'r str>) -> Self {
+    fn with_method(mut self, method: HttpMethod) -> Self {
+        self.method = method;
+        self
+    }
+
+    #[inline]
+    fn with_upstream(mut self, upstream: impl Into<Upstream<'r>>) -> Self {
+        self.upstream = upstream.into();
+        self
+    }
+
+    #[inline]
+    fn with_proxy(mut self, proxy: Option<&'r str>) -> Self {
         self.inner = self.inner.proxy(proxy);
         self
     }
 
-    /// Configure query for the request
     #[inline]
-    pub fn with_query(mut self, query: Option<Cow<'r, str>>) -> Self {
+    fn with_path(mut self, path: &'r str) -> Self {
+        self.path = path;
+        self
+    }
+
+    #[inline]
+    fn with_query(mut self, query: Option<Cow<'r, str>>) -> Self {
         self.query = query;
         self
     }
 
-    /// Configure headers for the request
     #[inline]
-    pub fn with_headers(mut self, headers: Option<impl Into<HttpHeaderMap>>) -> Self {
+    fn with_headers(mut self, headers: Option<impl Into<HttpHeaderMap>>) -> Self {
+        let headers = headers.map(|h| h.into());
         self.inner = self.inner.headers(headers);
         self
     }
 
-    /// Configure body for the request
     #[inline]
-    pub fn with_body(mut self, body: Option<impl Into<ReqBody>>) -> Self {
-        self.inner = self.inner.body(body);
+    fn with_headers_managed(mut self, headers: Option<impl Into<ManagedHeaderMap>>) -> Self {
+        self.inner = self.inner.headers(headers.map(|h| h.into().take_inner()));
         self
     }
 
     #[inline]
-    #[tracing::instrument(level = "debug", name = "GeneralRequest.execute", skip_all, err)]
-    pub async fn execute(self) -> Result<RawResponseExt> {
+    fn with_body(mut self, body: Option<impl Into<ReqBody>>) -> Self {
+        self.inner = self.inner.body(body);
+        self
+    }
+
+    #[tracing::instrument(level = "debug", name = "GeneralRpc.execute", skip_all, err)]
+    async fn execute(self) -> Result<Self::Response> {
         let full_url = self.upstream.url().map(|mut u: Url| {
             u.set_path(self.path);
             u.set_query(self.query.as_deref());
